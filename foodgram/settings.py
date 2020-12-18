139ae3f35280d29647de995e8a58ca5270f63795
Sergey <blackwolfsm@gmail.com>
2020-12-18 18:58:55 +0500

    add pages for auth

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'recipes',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -100,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
