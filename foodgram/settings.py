bb4f6c267126ff81779312dc32018d6633d76dba
Sergey <blackwolfsm@gmail.com>
2021-02-03 13:59:37 +0500

    move static, change setting for postgresSQL, create dockerfile for foodgram and nginx, create dockercompose

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '0f(ug=ol_g7bu16-gai4zfbvwe(y&7f=jogt(5!1)szawu^a!t'
 
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -63,8 +63,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
